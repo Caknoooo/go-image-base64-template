f49506d8f7f5f1099a91ccaba08fd776ec2f55ac
Caknoooo <caknocomel@gmail.com>
2023-08-22 06:22:28 +0700

    feat: setup database connection

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"golang-base64-file-encryption-template/config"
+
+	"gorm.io/gorm"
+)
+
+func main() {
+	var (
+		db *gorm.DB = config.SetupDatabaseConnection()
+	)
+
+	fmt.Println(db)
+}
