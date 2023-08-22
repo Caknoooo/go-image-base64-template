d04eea94f0e8019153d50cb9c3b64c135795e68b
Caknoooo <caknocomel@gmail.com>
2023-08-22 08:08:45 +0700

    feat: create service image create and get

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"golang-base64-file-encryption-template/config"
+	"golang-base64-file-encryption-template/controller"
+	"golang-base64-file-encryption-template/repository"
+	"golang-base64-file-encryption-template/service"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func main() {
 	var (
 		db *gorm.DB = config.SetupDatabaseConnection()
+		imageRepository repository.ImageRepository = repository.NewImageRepository(db)
+		imageService service.ImageService = service.NewImageService(imageRepository)
+		imageController controller.ImageController = controller.NewImageController(imageService)
 	)
 
-	fmt.Println(db)
+	server := gin.Default()
+	server.POST("/upload", imageController.UploadImage)
+	server.GET("/image/storage/:id", imageController.GetImage)
+
+	server.Run(":8080")
 }
